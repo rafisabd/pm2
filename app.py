2cf84934a2642e8a5d502479d882b116bccb143a
rafisabd <rafiahmed.sabd@gmail.com>
2025-07-17 16:50:04 +0600

    Create app.py
    app.py

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask, jsonify
+import requests
+
+app = Flask(__name__)
+
+@app.route('/pm02', methods=['GET'])
+def get_pm02():
+    try:
+        url = "https://api.airgradient.com/public/api/v1/locations/164620/measures/current?token=90e01a45-09c0-461e-acad-b88ec065e6ab"
+        response = requests.get(url, timeout=5)
+        response.raise_for_status()
+        data = response.json()
+        return jsonify({"pm02": data["pm02"]})
+    except Exception as e:
+        return jsonify({"error": "pm02 can't be loaded from render"}), 502
+
+if __name__ == '__main__':
+    app.run()
